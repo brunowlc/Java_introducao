a048d3d8d3c3acf90e7957fa6c4b4338ecdc7f6d
Bruno Wellington <brunowellington9@gmail.com>
2023-08-09 22:10:23 -0300

    Segundo commit: Operadore Ternario simples

--- java_introducao/src/operacoes_logicas/OperadorTernario.java
+++ java_introducao/src/operacoes_logicas/OperadorTernario.java
@@ -1,5 +1,22 @@
 package operacoes_logicas;
 
 public class OperadorTernario {
-
+	
+	public static void main(String[] args) {
+		
+		int nota1 = 10;
+		int nota2 = 60;
+		int nota3 = 70;
+		int nota4 = 80;
+		int media = 0;
+		String saida;
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		/*Tipo de condição usada para micro validações*/
+		/*saida = media >= 70 ? "Aluno Aprovado" : "Aluno Reprovado";*/
+		saida = media >= 70 ? "Aluno Aprovado" : (media >= 40 && media <=69) ? "Aluno em Recuperação" : "Aluno Reprovado";
+		/*A media é >= 70? se for verdade entao "aluno aprovado", : se nao a media é >=40 e <= 69? se for
+		verdade "Aluno em recuperação", : se nao cair em nem uma condição dessa "Aluno Reprovado". */
+		System.out.println(saida);
+	}
 }
